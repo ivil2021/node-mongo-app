e4353124b1416a578608199e0223692830fc68e4
ivil2021 <89508514326@mail.ru>
2021-04-30 16:24:40 +0300

    3 commit

--- models/Todo.js
+++ models/Todo.js
@@ -1,3 +1,14 @@
 const { Schema, model } = require("mongoose");
 
-const scheme = new Schema({});
+const schema = new Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  completed: {
+    type: Boolean,
+    default: false,
+  },
+});
+
+module.exports = model("Todo", schema);
--- routes/todos.js
+++ routes/todos.js
@@ -1,18 +1,31 @@
 const { Router } = require("express");
+const Todo = require("../models/Todo");
 const router = Router();
 
-router.get("/", (req, res) => {
+router.get("/", async (req, res) => {
+  const todos = await Todo.find({});
+
   res.render("index", {
     title: "Todos list",
-    isIndex: true
+    isIndex: true,
+    todos,
   });
 });
 
 router.get("/create", (req, res) => {
   res.render("create", {
     title: "Todos todo",
-    isCreate: true
+    isCreate: true,
   });
+});
+
+router.post("/create", async (req, res) => {
+  const todo = new Todo({
+    title: req.body.title,
+  });
+
+  await todo.save();
+  res.redirect("/");
 });
 
 module.exports = router;
